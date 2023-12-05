a613b35ce715fa5b26dbf73d03824e0b173c9535
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-05 11:12:49 +0530

    Code Releted To Exception Handling

--- Object_Oriented_Programming/Assignment_Question/Pin.java
+++ Object_Oriented_Programming/Assignment_Question/Pin.java
@@ -1,15 +1,16 @@
+/*Author :- Aditya Yadav */
 import java.util.*;
-class WrongPinException extends Exception
+class WrongPinException extends Exception //Throwing Custom Exception
 {
     public WrongPinException(String message)
     {
-        super(message);
+        super(message); //Printing Custom Message For Exception
     }
 }
-public class Pin
+public class Pin //Code About Exception Handling and Checking Pin
 {
-    final int pin=1234;
-    public static boolean check(int x)
+    final int pin=1234; //A Default Pin
+    public static boolean check(int x) //Function To Check Whether The Pin is Correct or Not
     {
         boolean re=false;
         Pin obj = new Pin();
@@ -26,31 +27,31 @@ public class Pin
         int count=0;
         do
         {
-            System.out.print("Enter the Pin :- ");
+            System.out.print("Enter the Pin :- "); //Taking Users Pin
             pin_e=in.nextInt();
-            if(check(pin_e)==false)
+            if(check(pin_e)==false) //Checking Whether The Pin is Correct Or Not
             {
-                count++;
+                count++; //Counting the Number Of Wrong Try
                 if(count!=3)
                 {
-                    System.out.println("Wrong Pin . ");
-                    System.out.println("Remaining Try :- "+(3-count));
+                    System.out.println("Wrong Pin . "); //Telling the User That he has Enter Wrong Pin
+                    System.out.println("Remaining Try :- "+(3-count)); //Printing Remaining Try
                 }
             }
             else
             {
-                System.out.println("Access granted! User information displayed . ");
+                System.out.println("Access granted! User information displayed . "); //If He Enter Right Pin Printing The Fruitfull Message
             }
             try
             {
-                if(count>=3)
+                if(count>=3) //If the Try Limit Exceeds Then Throwing the Exception
                 {
                     throw new WrongPinException("Account Blocked Temporary . ");
                 }
             }
             catch(Exception e)
             {
-                System.out.println("Error :- "+e.getMessage());
+                System.out.println("Error :- "+e.getMessage()); //Printing the Error to User
                 break;
             }
         }while(check(pin_e)!=true);
