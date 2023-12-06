7e7c4897ca89e35b9062e0230e25e7911a037a4f
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-06 10:18:19 +0530

    Code For Basic Banking System

--- Object_Oriented_Programming/Assignment_Question/Banking_System.java
+++ Object_Oriented_Programming/Assignment_Question/Banking_System.java
@@ -1,58 +1,59 @@
-import java.util.*;
-class InsufficientFundsException extends Exception
+/*Author :- Aditya Yadav */
+import java.util.*; //Code To Do Basic Operation of Banking System
+class InsufficientFundsException extends Exception //Custom Exception Class
 {
     public InsufficientFundsException(String message)
     {
         super(message);
     }
 }
-class BankAccount
+class BankAccount //Class Containing all Banking System Functions
 {
-    int acc_bal;
-    String acc_hol_name;
-    public BankAccount(int am,String str)
+    int acc_bal; //To Store Balance
+    String acc_hol_name; //Account Holder Name
+    public BankAccount(int am,String str) //Constructor
     {
         acc_bal=am;
         acc_hol_name=str;
     }
-    public void deposit(int am)
+    public void deposit(int am) //Function to Deposit Ammount
     {
         acc_bal+=am;
         System.out.println("Amount Deposit Successfully . ");
     }
-    public void withdraw(int am)
+    public void withdraw(int am) //Function To Withdraw
     {
         try
         {
-            if(acc_bal<am)
+            if(acc_bal<am) //If the Requested balance is High Then Throwing a Custom Exception
             {
                 throw new InsufficientFundsException("Insufficient funds for the withdrawal . ");
             }
-            else
+            else //If Condition False then Withdrawing the Requested amount
             {
                 acc_bal-=am;
                 System.out.println("Amount Withdrawn . ");
                 System.out.println("Account Balance :- "+acc_bal);
             }
         }
-        catch(Exception e)
+        catch(Exception e)//Catching the Thrown Exception
         {
             System.out.println("Error :- "+e.getMessage());
         }
     }
 }
-public class Banking_System
+public class Banking_System //Main Class To Implement the Banking System
 {
     public static void main(String[]args)
     {
         int amount;
-        BankAccount obj = new BankAccount(1000,"User_1");
-        Scanner in = new Scanner(System.in);
+        BankAccount obj = new BankAccount(1000,"User_1"); //Create a Obj For any Customer
+        Scanner in = new Scanner(System.in); //Giving the User Choice
         System.out.println("1.Deposit . ");
         System.out.println("2.Withdraw . ");
         System.out.print("Enter Your Choice :- ");
         int ch=in.nextInt();
-        if(ch==1)
+        if(ch==1) //performing the operation according to the user choice
         {
             System.out.print("Enter the Amount To Deposit :- ");
             amount=in.nextInt();
