a62ea99754b55be69aa59ed7d43e658d84351b60
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-07 21:55:38 +0530

    Banking System With Multithread

--- Object_Oriented_Programming/Assignment_Question/Banking_System_Multithread.java
+++ Object_Oriented_Programming/Assignment_Question/Banking_System_Multithread.java
@@ -1,72 +1,72 @@
 /*Author :- Aditya Yadav */
 import java.util.*;
-class InsufficientFundsException extends Exception
+class InsufficientFundsException extends Exception //Custom Exception Class For Insufficient Funds
 {
-    public InsufficientFundsException(String str)
+    public InsufficientFundsException(String str) //Constructor to create a Custom Message
     {
         super(str);
     }
 }
-class Invaliddeposit extends Exception
+class Invaliddeposit extends Exception //Custom Exception Class For Negative Deposit
 {
-    public Invaliddeposit(String str)
+    public Invaliddeposit(String str) //Constructor to create a Custom Message
     {
         super(str);
     }
 }
-class Account
+class Account //Class To Implement Banking system with Different Funds
 {
     int bal;
-    public Account(int bal)
+    public Account(int bal) //Costructor to Initialize Balance
     {
         this.bal=bal;
     }
-    public boolean isSufficientBalance(int amt)
+    public boolean isSufficientBalance(int amt) //Function to Check wheather Balance is Sufficient or Not
     {
         if(bal>=amt)
         {
-            return true;
+            return true; //Return true if Sufficient Amount is there to Withdraw
         }
         else
         {
             return false;
         }
     }
-    public void withdraw(int amount)
+    public void withdraw(int amount) //Function to Withdraw
     {
         bal=bal-amount;
         System.out.println("Amount Withdrawn . ");
         System.out.println("Balance Remaining :- "+bal);
     }
-    public void deposit(int amount)
+    public void deposit(int amount) //Function to Deposit
     {
         bal=bal+amount;
         System.out.println("Amount Deposited . ");
         System.out.println("Current Balance :- "+bal);
     }
 }
-class Customer implements Runnable
+class Customer implements Runnable //Implementing Thread Using The Runnable Interface
 {
     String name;
     Account acc;
-    public Customer(Account acc,String name)
+    public Customer(Account acc,String name) //Custructor To Initizalise the name and account class Reference
     {
         this.acc=acc;
         this.name=name;
     }
-    public void run()
+    public void run() //Thread Code Run After Creation
     {
-        synchronized(acc)
+        synchronized(acc) //Synchronized Block So That One Thread Can Access The Critical Section
         {
             Scanner in = new Scanner(System.in);
             System.out.println("1.Deposit . ");
             System.out.println("2.Withdraw . ");
-            System.out.print("Hello "+name+" Please Enter Your Choice :- ");
+            System.out.print("Hello "+name+" Please Enter Your Choice :- "); //Taking User Choice
             int ch=in.nextInt();
             int amt;
             if(ch==1)
             {
-                System.out.print("Enter Amount To Deposit :- ");
+                System.out.print("Enter Amount To Deposit :- "); //Depositing Amount
                 amt=in.nextInt();
                 try
                 {
@@ -76,17 +76,17 @@ class Customer implements Runnable
                     }
                     else
                     {
-                        throw new Invaliddeposit("The Depositing Amount Can't be Negative Or Zero ");
+                        throw new Invaliddeposit("The Depositing Amount Can't be Negative Or Zero "); //Throwing Exception
                     }
                 }
-                catch(Exception e)
+                catch(Exception e) //Catching the Thrown Exception
                 {
                     System.out.println("Error :- "+e.getMessage());
                 }
             }
             else if(ch==2)
             {
-                System.out.print("Enter Amount To Withdraw :- ");
+                System.out.print("Enter Amount To Withdraw :- "); //Withdrawing Amount
                 amt=in.nextInt();
                 try
                 {
@@ -97,10 +97,10 @@ class Customer implements Runnable
                     }
                     else
                     {
-                        throw new InsufficientFundsException("Insufficient Balance Can't Withdraw Requested Ammount . ");
+                        throw new InsufficientFundsException("Insufficient Balance Can't Withdraw Requested Ammount . "); //Throwing Exception
                     }
                 }
-                catch(Exception e)
+                catch(Exception e) //Catching the Thrown Exception
                 {
                     System.out.println("Error :- "+e.getMessage());
                 }
@@ -108,16 +108,16 @@ class Customer implements Runnable
         }
     }
 }
-public class Banking_System_Multithread
+public class Banking_System_Multithread //Main Class To Implement The Multithread Synchronization
 {
     public static void main(String[]args)
     {
-        Account a1 = new Account(1000);
-        Customer c1 = new Customer(a1,"Varun");
-        Customer c2 = new Customer(a1,"Vansh");
+        Account a1 = new Account(1000); //Account Use as a Shared Variable
+        Customer c1 = new Customer(a1,"Varun"); //Thread One
+        Customer c2 = new Customer(a1,"Vansh"); //Thread Two
         Thread t1 = new Thread(c1);
         Thread t2 = new Thread(c2);
-        t1.start();
-        t2.start();
+        t1.start(); //Creating and Starting Thread
+        t2.start(); //Creating and Starting Thread
     }
 }
