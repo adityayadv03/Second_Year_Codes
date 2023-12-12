e9b15877d668ffb241cd534ce735d729331e2acc
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-12 19:19:49 +0530

    Experiment 2 Question 3

--- Object_Oriented_Programming/Course_Experiment/Experiment_2/Question_3.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_2/Question_3.java
@@ -1,20 +1,20 @@
 /*Author :- Aditya Yadav */
 import java.util.*;
-public class Question_3
+public class Question_3 //Program to find the Average of 5 Number
 {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
         int arr[]=new int[5];
         int sum=0;
-        System.out.print("Enter Five Element :- ");
+        System.out.print("Enter Five Element :- "); //Taking Input 
         for(int i=0 ; i<5 ; i++)
         {
             arr[i]=in.nextInt();
-            sum=sum+arr[i];
+            sum=sum+arr[i]; //Calculating the Sum for average
         }
-        float avg=sum/5;
-        System.out.println("The Average of the Given Number is :- "+avg);
+        float avg=sum/5; //Calculating the Average
+        System.out.println("The Average of the Given Number is :- "+avg); //Printing the average
         in.close();
     }
 }
