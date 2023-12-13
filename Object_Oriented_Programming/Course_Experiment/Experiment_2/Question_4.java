af4eeeb6ad81db7bf0b7a2f108daee919cb94ec0
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-13 18:51:56 +0530

    Experiment 2 Question 4

--- Object_Oriented_Programming/Course_Experiment/Experiment_2/Question_4.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_2/Question_4.java
@@ -1,9 +1,10 @@
-public class Question_4
+/*Author :- Aditya Yadav */
+public class Question_4 //Program to implement a method of Wrapper class
 {
     public static void main(String[] args)
     {
         String str="107292.903";
-        double d=Double.parseDouble(str);
-        System.out.println("The Value of d is :- "+d);
+        double d=Double.parseDouble(str); //parseDouble help in converting string to double value
+        System.out.println("The Value of d is :- "+d); //Printing the converted Double Value
     }
 }
