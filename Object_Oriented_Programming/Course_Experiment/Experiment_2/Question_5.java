3b95fe8dde4c1467cec2041120424b247a152f67
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-14 10:35:57 +0530

    Experiment 2 Question 5

---
+++ Object_Oriented_Programming/Course_Experiment/Experiment_2/Question_5.java
@@ -0,0 +1,10 @@
+/*Author :- Aditya Yadav */
+public class Question_5
+{
+    public static void main(String[]args)
+    {
+        String str="903";
+        int a=Integer.parseInt(str);
+        System.out.println("The Value of a is :- "+a);
+    }
+}
