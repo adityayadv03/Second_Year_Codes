0ae10ae473c81b8d6001ef5cf5a54da0bd687a9b
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-14 10:37:25 +0530

    Experiment 2 Question 5

--- Object_Oriented_Programming/Course_Experiment/Experiment_2/Question_5.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_2/Question_5.java
@@ -1,10 +1,10 @@
 /*Author :- Aditya Yadav */
-public class Question_5
+public class Question_5 //Program to implement a method of Wrapper class
 {
     public static void main(String[]args)
     {
         String str="903";
-        int a=Integer.parseInt(str);
-        System.out.println("The Value of a is :- "+a);
+        int a=Integer.parseInt(str); //parseInt help in converting string to int value
+        System.out.println("The Value of a is :- "+a); //Printing the converted integer Value
     }
 }
