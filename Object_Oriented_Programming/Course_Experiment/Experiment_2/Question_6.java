9f0b6f307ef1a2f22b1a15639e3f905de16c784d
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-15 18:56:48 +0530

    Experiment 2 Question 6

--- Object_Oriented_Programming/Course_Experiment/Experiment_2/Question_6.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_2/Question_6.java
@@ -1,9 +1,10 @@
-public class Question_6
+/*Author :- Aditya Yadav */
+public class Question_6 //Program to Convert Integer to String
 {
     public static void main(String[]args)
     {
-        Integer a=1000;
-        String str=Integer.toString(a);
-        System.out.println("Value of str is :- "+str);
+        Integer a=1000; //Integer Value
+        String str=Integer.toString(a); //Converting it to string
+        System.out.println("Value of str is :- "+str); //Printing its value
     }
 }
