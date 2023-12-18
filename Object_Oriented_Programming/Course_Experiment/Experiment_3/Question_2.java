2c1f69ca61aa123c3d189e1e6d12265491a923d7
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-18 19:09:20 +0530

    Experiment 3 Question 2

---
+++ Object_Oriented_Programming/Course_Experiment/Experiment_3/Question_2.java
@@ -0,0 +1,11 @@
+package Second_Year_Codes.Object_Oriented_Programming.Course_Experiment.Experiment_3;
+/*Author :- Aditya Yadav */
+public class Question_2
+{
+    public static void main(String[] args)
+    {
+        int a=Integer.parseInt(args[0]);
+        int b=Integer.parseInt(args[1]);
+        System.out.println("Sum :- "+(a+b));
+    }
+}
--- Object_Oriented_Programming/Course_Experiment/Experiment_3/readme.md
+++ Object_Oriented_Programming/Course_Experiment/Experiment_3/readme.md
@@ -6,6 +6,6 @@
     <h>Experiment_3</h>
     <p>Experiment 3 Basic Java Programming </p>
     <p>Question 1 Write a Program to Find the Largest of 3 number . </p>
-    <p>Question 2 </p>
+    <p>Question 2 Write a Program to add two number using Command line arguments . </p>
 </body>
 </html>
