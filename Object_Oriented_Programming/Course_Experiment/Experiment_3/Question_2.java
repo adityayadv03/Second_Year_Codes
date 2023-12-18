1998a0873dc04e7898633b9815d9a88394d39ceb
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-18 19:11:25 +0530

    Experiment 3 Question 2

--- Object_Oriented_Programming/Course_Experiment/Experiment_3/Question_2.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_3/Question_2.java
@@ -1,11 +1,11 @@
 package Second_Year_Codes.Object_Oriented_Programming.Course_Experiment.Experiment_3;
 /*Author :- Aditya Yadav */
-public class Question_2
+public class Question_2 //Program to Add Two Number from command Line Arguments
 {
-    public static void main(String[] args)
+    public static void main(String[] args) //When we run code in command line after the Command the number are given and store in args array
     {
-        int a=Integer.parseInt(args[0]);
-        int b=Integer.parseInt(args[1]);
-        System.out.println("Sum :- "+(a+b));
+        int a=Integer.parseInt(args[0]); //Converting the arguments to Interger
+        int b=Integer.parseInt(args[1]); //Converting the arguments to Interger
+        System.out.println("Sum :- "+(a+b)); //Printing the Sum
     }
 }
