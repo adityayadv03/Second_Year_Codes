10363d2ce95d06e4f3521cbb15c9b01f98e4c103
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-20 18:52:03 +0530

    Experiment 3 Question 4

--- Object_Oriented_Programming/Course_Experiment/Experiment_3/Question_4.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_3/Question_4.java
@@ -4,22 +4,22 @@ class person
 {
     String name;
     int age;
-    person(String str,int year)
+    person(String str,int year) //Constructor
     {
         name=str;
         age=year;
     }
-    void display()
+    void display() //Display Function
     {
         System.out.println("Name :- "+name);
         System.out.println("Age :- "+age);
     }
 }
-public class Question_4 
+public class Question_4 //Program to Implement Class and Objects
 {
     public static void main(String[] args)
     {
-        person pr1 = new person("Aditya",19);
+        person pr1 = new person("Aditya",19); //Object Creation of Class person
         pr1.display();
     }
 }
