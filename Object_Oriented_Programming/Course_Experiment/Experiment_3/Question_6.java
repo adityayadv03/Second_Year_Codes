af04f4a02bbeadaf5a34ec6939b3f18308aab3c9
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-22 13:16:21 +0530

    Experiment 3 Completed

--- Object_Oriented_Programming/Course_Experiment/Experiment_3/Question_6.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_3/Question_6.java
@@ -1,27 +1,27 @@
 package Second_Year_Codes.Object_Oriented_Programming.Course_Experiment.Experiment_3;
 /*Author :- Aditya Yadav */
 import java.util.*;
-public class Question_6
+public class Question_6 //Program to Find the Square of ten Number and Store their Sum
 {
     public static void main(String[] args) 
     {
         Scanner in = new Scanner(System.in);
         int arr[]=new int[10];
         int temp,sum=0;
-        System.out.print("Enter Ten Element :- ");
+        System.out.print("Enter Ten Element :- "); //Taking 10 Number From User
         for(int i=0 ; i<10 ; i++)
         {
             temp=in.nextInt();
-            arr[i]=temp*temp;
-            sum=sum+arr[i];
+            arr[i]=temp*temp; //Calculating their Sum
+            sum=sum+arr[i]; //Calculating their Sum
         }
-        System.out.print("Array After Updation :- ");
+        System.out.print("Array After Updation :- "); //Printing thier Updated Value
         for(int i=0 ; i<10 ; i++)
         {
             System.out.print(arr[i]+" ");
         }
         System.out.println();
-        System.out.println("Sum of Updated Element :- "+sum);
+        System.out.println("Sum of Updated Element :- "+sum); //Printing Their Sum
         in.close();
     }
 }
--- Object_Oriented_Programming/Course_Experiment/Experiment_3/Question_7.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_3/Question_7.java
@@ -1,17 +1,17 @@
 package Second_Year_Codes.Object_Oriented_Programming.Course_Experiment.Experiment_3;
 /*Author :- Aditya Yadav */
-public class Question_7 
+public class Question_7 //Program to Find Sum of All Number Deivsible by 5 In Range 40 to 250
 {
     public static void main(String[] args)
     {
         int sum=0;
-        for(int i=41 ; i<250 ; i++)
+        for(int i=41 ; i<250 ; i++) //Running The Loop In Given Range
         {
-            if(i%5==0)
+            if(i%5==0) //If the Number is Divisible Then Adding it in the Variable sum
             {
                 sum=sum+i;
             }
         }
-        System.out.println("Sum of All the Divisible in Given Range is :- "+sum);
+        System.out.println("Sum of All the Divisible in Given Range is :- "+sum); //Printing Their Sum
     }
 }
