a2433ad4ae4ae09b2942d74dfd6c0c847887688c
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-24 11:35:30 +0530

    Experiment 4 Question 5 and 6

--- Object_Oriented_Programming/Course_Experiment/Experiment_4/Question_6.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_4/Question_6.java
@@ -1,18 +1,18 @@
 package Second_Year_Codes.Object_Oriented_Programming.Course_Experiment.Experiment_4;
 /*Author :- Aditya Yadav */
 import java.util.*;
-public class Question_6 
+public class Question_6 //Program To Find and Implement Bitwise AND OR XOR Operator
 {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter First Number :- ");
+        System.out.print("Enter First Number :- "); //Taking two Number
         int a=in.nextInt();
         System.out.print("Enter Second Number :- ");
         int b=in.nextInt();
-        System.out.println("Bitwise AND Between a and b is :- "+(a&b));
-        System.out.println("Bitwise OR Between a and b is :- "+(a|b));
-        System.out.println("Bitwise XOR Between a and b is :- "+(a^b));
+        System.out.println("Bitwise AND Between a and b is :- "+(a&b)); //Bitwise AND Operator
+        System.out.println("Bitwise OR Between a and b is :- "+(a|b)); //Bitwise OR Operator
+        System.out.println("Bitwise XOR Between a and b is :- "+(a^b)); //Bitwise XOR Operator
         in.close();
     }
 }
