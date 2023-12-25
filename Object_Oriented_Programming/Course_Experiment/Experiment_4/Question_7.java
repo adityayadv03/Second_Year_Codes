479828adc61047f5f0f50e2634a89254092c48dc
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-25 17:48:57 +0530

    Experiment 4 Completed

--- Object_Oriented_Programming/Course_Experiment/Experiment_4/Question_7.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_4/Question_7.java
@@ -1,17 +1,17 @@
 package Second_Year_Codes.Object_Oriented_Programming.Course_Experiment.Experiment_4;
 /*Author :- Aditya Yadav */
 import java.util.*;
-public class Question_7
+public class Question_7 //Program to Implement Left and Right Shift Operator
 {
     public static void main(String[] args) 
     {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter First Number :- ");
+        System.out.print("Enter First Number :- "); //Taking a Number to Operate On
         int a=in.nextInt();
-        System.out.print("Enter Number of Bits to Shift :- ");
+        System.out.print("Enter Number of Bits to Shift :- "); //Taking Number of Bits
         int b=in.nextInt();
-        System.out.println("Left Shift of Given Number is :- "+(a<<b));
-        System.out.println("Right Shift of Given Number is :- "+(a>>b));
+        System.out.println("Left Shift of Given Number is :- "+(a<<b)); //Left Shift Operator
+        System.out.println("Right Shift of Given Number is :- "+(a>>b)); //Right Shift Operator
         in.close();
     }
 }
--- Object_Oriented_Programming/Course_Experiment/Experiment_4/Question_8.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_4/Question_8.java
@@ -1,15 +1,15 @@
 package Second_Year_Codes.Object_Oriented_Programming.Course_Experiment.Experiment_4;
 /*Author :- Aditya Yadav */
-class C
+class C //A Class To Implement the instanceod method
 {
     int a=100;
 }
-public class Question_8
+public class Question_8 //Program to implement instanceof Method
 {
     public static void main(String[] args)
     {
-        C obj = new C();
-        if(obj instanceof C)
+        C obj = new C(); //Creating a Instance of Class C
+        if(obj instanceof C) //Checking whether the instance is of Class C with instanceof Method
         {
             System.out.println("Obj is the Object of C Class . ");
         }
