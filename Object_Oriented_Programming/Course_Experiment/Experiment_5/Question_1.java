12b3435ae9c92ed87d4ebec6755180cbeb19123a
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-26 18:02:58 +0530

    Experiment 5

--- Object_Oriented_Programming/Course_Experiment/Experiment_5/Question_1.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_5/Question_1.java
@@ -1,12 +1,12 @@
 package Second_Year_Codes.Object_Oriented_Programming.Course_Experiment.Experiment_5;
 /*Author :- Aditya Yadav */
-interface test
+interface test //Interface test Having method Declaration
 {
     int square(int x);
 }
-class arithmetic implements test
+class arithmetic implements test //A Class implementing the interface
 {
-    public int square(int x)
+    public int square(int x) //Defining that method
     {
         x=x*x;
         return x;
@@ -16,7 +16,7 @@ public class Question_1 //Class Name Should be ToTestInt According to Question
 {
     public static void main(String[] args)
     {
-        arithmetic obj = new arithmetic();
+        arithmetic obj = new arithmetic(); //Creating the Object of the class implementing the interface
         System.out.println("Square of 2 is :- "+obj.square(2));
     }
 }
--- Object_Oriented_Programming/Course_Experiment/Experiment_5/Question_2.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_5/Question_2.java
@@ -1,18 +1,18 @@
 package Second_Year_Codes.Object_Oriented_Programming.Course_Experiment.Experiment_5;
 /*Author :- Aditya Yadav */
-interface A
+interface A //Interface test Having method Declaration
 {
     int meth1(int a,int b);
     int meth2(int a,int b);
 }
-class My implements A
+class My implements A //A Class implementing the interface
 {
-    public int meth1(int a,int b)
+    public int meth1(int a,int b) //Defining that method
     {
         a=a+b;
         return a;
     }
-    public int meth2(int a,int b)
+    public int meth2(int a,int b) //Defining that method
     {
         a=a*b;
         return a;
@@ -22,7 +22,7 @@ public class Question_2 //Class Name Should be MyClass According to Question
 {
     public static void main(String[] args)
     {
-        My obj = new My();
+        My obj = new My(); //Creating the Object of the class implementing the interface
         System.out.println("Sum of 2 and 3 is :- "+obj.meth1(2,3));
         System.out.println("Multiple of 2 and 3 is :- "+obj.meth2(2,3));
     }
