e4e6dfcba6b9655ea99d1bb8168e5e0ddf4832fe
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-27 18:15:47 +0530

    Experiment 5 Completed

--- Object_Oriented_Programming/Course_Experiment/Experiment_5/Question_3.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_5/Question_3.java
@@ -1,23 +1,23 @@
 package Second_Year_Codes.Object_Oriented_Programming.Course_Experiment.Experiment_5;
 /*Author :- Aditya Yadav */
-interface constant
+interface constant //Interface to Implement
 {
     double pi=3.14;
     void ar(int r);
 }
-class Area implements constant
+class Area implements constant //Class Implementing the Interface
 {
-    public void ar(int r)
+    public void ar(int r) //Defining the Function declare in interface
     {
-        float a=(float)(pi*r*r);
-        System.out.println("The Radius Of Circle is :- "+a);
+        float a=(float)(pi*r*r); //Calculating the Area
+        System.out.println("The Radius Of Circle is :- "+a); //Printing the Calculated Area
     }
 }
 public class Question_3
 {
     public static void main(String[] args)
     {
-        Area obj = new Area();
-        obj.ar(7);
+        Area obj = new Area(); //Creating the object of the Class
+        obj.ar(7); //Calling the function for calculating area
     }
 }
--- Object_Oriented_Programming/Course_Experiment/Experiment_5/Question_4.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_5/Question_4.java
@@ -1,29 +1,29 @@
 package Second_Year_Codes.Object_Oriented_Programming.Course_Experiment.Experiment_5;
 /*Author :- Aditya Yadav */
-interface two
+interface two //Creating the interface to declare two function div and mod
 {
-    void div(int a,int b);
-    void mod(int a,int b);
+    void div(int a,int b); //function to calculate division between two number
+    void mod(int a,int b); //function to calculate modulus between two number
 }
-class Ride implements two
+class Ride implements two //Class to implementing the interface
 {
-    public void div(int a,int b)
+    public void div(int a,int b) //Function defination to calculate division
     {
-        float c=(float)(a/b);
-        System.out.println("The Division of "+a+" "+b+" is :- "+c);
+        float c=(float)(a/b); //Calculating the division
+        System.out.println("The Division of "+a+" "+b+" is :- "+c); //Printing the Division
     }
-    public void mod(int a,int b)
+    public void mod(int a,int b) //Function defination to calculate modulus
     {
-        int c=a%b;
-        System.out.println("The Modulus of "+a+" "+b+" is :- "+c);
+        int c=a%b; //Calculating the Modulus
+        System.out.println("The Modulus of "+a+" "+b+" is :- "+c); //Printing the Modulus
     }
 }
 public class Question_4
 {
     public static void main(String[] args)
     {
-        Ride obj = new Ride();
-        obj.div(3,2);
+        Ride obj = new Ride(); // Creating the Object of the Class
+        obj.div(3,2); //Calling the Required Functions
         obj.mod(3,2);
     }
 }
