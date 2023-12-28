014cecfcae05e42c0545245d9799fb066f2d4044
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-28 19:36:11 +0530

    Experiment 7

--- Object_Oriented_Programming/Course_Experiment/Experiment_7/Question_1.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_7/Question_1.java
@@ -1,49 +1,49 @@
 package Second_Year_Codes.Object_Oriented_Programming.Course_Experiment.Experiment_7;
 /*Author :- Aditya Yadav */
 import java.util.*;
-class Record
+class Record //Class to hold the Student details and require Function
 {
-    String name;
-    int roll;
-    public Record(String name,int roll)
+    String name; //Variable to Store Name
+    int roll; //Variable to Store Roll Number
+    public Record(String name,int roll) //Constructor to assign the details
     {
         this.name=name;
         this.roll=roll;
     }
-    void display()
+    void display() //Function to display the details
     {
         System.out.println("Name :- "+name);
         System.out.println("Roll Number :- "+roll);
     }
 }
-public class Question_1
+public class Question_1 //Program to Handle exception Cause During accessing the details of Students(Generally ArrayOutOfBoundException)
 {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the Number Of Students :- ");
-        int n=in.nextInt();
-        Record arr[] = new Record[n];
-        String na;
-        int r;
+        System.out.print("Enter the Number Of Students :- "); //Taking the Number of Students from the User
+        int n=in.nextInt(); 
+        Record arr[] = new Record[n]; //Creating the array of Object to store the details of each Students
+        String na; //Variable to Store Name
+        int r; //Variable to Store Roll Number
         for(int i=0 ; i<n ; i++)
         {
-            System.out.print("Enter Name :- ");
+            System.out.print("Enter Name :- "); //Taking the Name 
+            na=in.nextLine(); //Used this statement two time as it gets ignore if we write its onces
             na=in.nextLine();
-            na=in.nextLine();
-            System.out.print("Enter Roll Number :- ");
+            System.out.print("Enter Roll Number :- "); //Taking the Roll Number
             r=in.nextInt();
-            arr[i]=new Record(na,r);
+            arr[i]=new Record(na,r); //Intizialising the object with the name and roll number
         }
         try
         {
-            System.out.print("Enter the Number Of Student To View Details :- ");
+            System.out.print("Enter the Number Of Student To View Details :- "); //Asking the Number of Students to display the details
             int a=in.nextInt();
-            arr[a-1].display();
+            arr[a-1].display(); //Calling the display function with a-1 as converting the position to index
         }
-        catch(Exception e)
+        catch(Exception e) //Catching the Exception Caused
         {
-            System.out.println("Error :- "+e.getMessage());
+            System.out.println("Error :- "+e.getMessage()); //Printing the Error Using the getMessage Function
         }
         in.close();
     }
--- Object_Oriented_Programming/Course_Experiment/Experiment_7/Question_2.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_7/Question_2.java
@@ -1,23 +1,23 @@
 package Second_Year_Codes.Object_Oriented_Programming.Course_Experiment.Experiment_7;
 /*Author :- Aditya Yadav */
 import java.util.*;
-public class Question_2
+public class Question_2 //Program to handle exception cause by division by 0
 {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter Any Number :- ");
+        System.out.print("Enter Any Number :- "); //Taking the First Number
         int a=in.nextInt();
-        System.out.print("Enter Any Number :- ");
+        System.out.print("Enter Any Number :- "); //Taking the Second Number
         int b=in.nextInt();
         in.close();
         try
         {
-            System.out.println("Division :- "+(a/b));
+            System.out.println("Division :- "+(a/b)); //If the Division Cause any Error
         }
-        catch(Exception e)
+        catch(Exception e) //Exception catch from the try block
         {
-            System.out.println("Error Occur :- "+e.getMessage());
+            System.out.println("Error Occur :- "+e.getMessage()); //Then Printing the Error Using the getMessage function
         }
     }
 }
