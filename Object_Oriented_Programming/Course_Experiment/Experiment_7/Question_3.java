6666d1961d64c22dab568e456373d68c81dc6e31
adityayadv03 <officialuser.aditya2022@gmail.com>
2023-12-29 15:31:17 +0530

    Experiment 7 Completed

--- Object_Oriented_Programming/Course_Experiment/Experiment_7/Question_3.java
+++ Object_Oriented_Programming/Course_Experiment/Experiment_7/Question_3.java
@@ -1,18 +1,18 @@
 package Second_Year_Codes.Object_Oriented_Programming.Course_Experiment.Experiment_7;
 /*Author :- Aditya Yadav */
-public class Question_3
+public class Question_3 //program to check if the operand performing modulus is nonnumeric or not
 {
     public static void main(String[] args)
     {
         try
         {
-            int a=Integer.parseInt(args[0]);
-            int b=Integer.parseInt(args[1]);
+            int a=Integer.parseInt(args[0]); //Converting the string argument to int
+            int b=Integer.parseInt(args[1]); //If the argument are nonnumeric it will create a exception which is get catch by catch block
             System.out.println("Modulus is :- "+(a%b));
         }
         catch(Exception e)
         {
-            System.out.println("Error is :- "+e.getMessage());
+            System.out.println("Error is :- "+e.getMessage()); //Showing the message of exception with the help if getMessage Function
         }
     }
 }
