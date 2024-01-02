202709fd158e33e155ddb8c58ff22a4266b56fa0
adityayadv03 <officialuser.aditya2022@gmail.com>
2024-01-02 15:00:38 +0530

    Bubble Sorting Completed

--- Design_And_Analysis_Of_Algorithms/Sorting_Algorithms/Bubble_Sort.c
+++ Design_And_Analysis_Of_Algorithms/Sorting_Algorithms/Bubble_Sort.c
@@ -1,29 +1,29 @@
 /*Author :- Aditya Yadav */
 #include<stdio.h> //Program to Sort Numbers in Given Array Using Bubble sort Technique
-int main()
+int main() //In Bubble sort a bubble of two number is check if the first element is greater than second if yes then swapping it
 {
-    int n,temp;
-    printf("Enter The Size of Array :- ");
+    int n,temp; // /-n To Store the size of array /-temp a temporary variable to perform swapping
+    printf("Enter The Size of Array :- "); //Taking the array size
     scanf("%d",&n);
-    int arr[n];
-    printf("Enter the Element :- ");
+    int arr[n]; //Declaring the array of n size 
+    printf("Enter the Element :- "); //Taking the array element from user
     for(int i=0 ; i<n ; i++)
     {
         scanf("%d",&arr[i]);
     }
-    for(int i=0 ; i<n ; i++)
+    for(int i=0 ; i<n ; i++) //Bubble Sorting Logic
     {
-        for(int j=0 ; j<n-1 ; j++)
+        for(int j=0 ; j<n-1 ; j++) //For traversing the array element to n-1 element as for n time it can generate ArrayIndexOutOfBound exception as because of the condition written in the logic
         {
-            if(arr[j]>arr[j+1])
-            {
+            if(arr[j]>arr[j+1]) //Checking the element present in the first position is greater or not than the second element
+            { //If Yes then Swapping it
                 temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
             }
         }
     }
-    printf("Array After Sorting :- ");
+    printf("Array After Sorting :- "); //Priting the Element After Sorting
     for(int i=0 ; i<n ; i++)
     {
         printf("%d ",arr[i]);
