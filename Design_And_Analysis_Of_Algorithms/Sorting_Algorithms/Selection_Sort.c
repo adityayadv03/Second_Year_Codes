06f72126edb1e7d7003eebc96494d27f64ec6ae3
adityayadv03 <officialuser.aditya2022@gmail.com>
2024-01-04 12:23:59 +0530

    Selection Sort

--- Design_And_Analysis_Of_Algorithms/Sorting_Algorithms/Selection_Sort.c
+++ Design_And_Analysis_Of_Algorithms/Sorting_Algorithms/Selection_Sort.c
@@ -1,27 +1,27 @@
 /*Author :- Aditya Yadav */
 #include<stdio.h> //Program to Sort Numbers in Given Array Using Selection sort Technique
-int main()
+int main() //In Selection Sort we Find the smallest number and swap with the Specific Position
 {
-    int n,temp;
-    printf("Enter The Size of Array :- ");
+    int n,temp; // /-n To Store the Size of Array /-temp to swap the Numbers 
+    printf("Enter The Size of Array :- "); //Taking the array Size
     scanf("%d",&n);
-    int arr[n]; 
-    printf("Enter the Element :- ");
+    int arr[n];  //Declaring the array of size n
+    printf("Enter the Element :- "); //Taking the array Element 
     for(int i=0 ; i<n ; i++)
     {
         scanf("%d",&arr[i]);
     }
-    for(int i=0 ; i<n ; i++)
+    for(int i=0 ; i<n ; i++) //Selection Sort Techniques
     {
-        int min=i;
-        for(int j=i ; j<n ; j++)
+        int min=i; //Assuming the first element to be the smallest and storing the index of that element
+        for(int j=i ; j<n ; j++) //running the loop to traverse the array and finding the smallest number then the min element
         {
-            if(arr[j]<arr[min])
+            if(arr[j]<arr[min]) //if any element is found smallest then the index of that element is store in min
             {
                 min=j;
             }
         }
-        temp=arr[min];
+        temp=arr[min]; //Aftering the whole array is traverse and smallest is found then swapping it with the current index of i
         arr[min]=arr[i];
         arr[i]=temp;
     }
