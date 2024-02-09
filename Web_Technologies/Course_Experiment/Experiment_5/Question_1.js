65362921535e7e0c9746c0ed38f3324e8ff6386d
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-02-09 22:54:41 +0530

    Learning some js things

---
+++ Web_Technologies/Course_Experiment/Experiment_5/Question_1.js
@@ -0,0 +1,14 @@
+// Author :- Aditya Yadav
+let n1=parseInt(prompt("Enter First Number :- "));
+let n2=parseInt(prompt("Enter Second Number :- "));
+
+if(n1>n2)
+{
+    alert(n1+" is Greater . ");
+    document.write(n1+" is Greater . ");
+}
+else
+{
+    alert(n2+" is Greater . ");
+    document.write(n2+" is Greater . ");
+}
