b004be2bb6137a4632648a12eac3bd8d2fb7f696
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-02-22 22:28:19 +0530

    Question 5 of Experiment 5

---
+++ Web_Technologies/Course_Experiment/Experiment_5/Question_5.js
@@ -0,0 +1,9 @@
+// Author :- Aditya Yadav
+let choice=prompt("Enter Any Number From 1 To 100 :- ")
+if(choice<=0 || choice>100){
+    alert("Wrong Choice")
+}else if(choice%2==0){
+    alert("The Given Number is Even . ")
+}else{
+    alert("The Given Number is Odd . ")
+}
