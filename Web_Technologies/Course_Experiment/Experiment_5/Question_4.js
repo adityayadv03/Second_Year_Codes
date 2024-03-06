cb4898a2bae381c730ed776d52818d597596c0de
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-03-06 18:46:53 +0530

    Adding some Comment

--- Web_Technologies/Course_Experiment/Experiment_5/Question_4.js
+++ Web_Technologies/Course_Experiment/Experiment_5/Question_4.js
@@ -1,13 +1,12 @@
-let str=prompt("Enter any String :- ");
-str=str.split(" ");
-let idx=-1
+// Author :- Aditya Yadav
+let str=prompt("Enter any String :- "); //Taking Input from Prompt
+str=str.split(" "); //Spliting Each Word having space in between
+let idx=-1 //Created a Variable to Store the index of longest String
 let paraEL=document.getElementById("para-el");
-for(let i=0 ; i<str.length ; i++){
-    if(i==0 || str[idx].length<str[i].length){
-        idx=i;
+for(let i=0 ; i<str.length ; i++){ //Traverse the Split String Array
+    if(i==0 || str[idx].length<str[i].length){ //Check the Condition and words having high length
+        idx=i; //Storing it in idx
     }
 }
 paraEL.innerHTML="<p style='display:inline'>The Longest Word in the String is :- </p>"
-paraEL.innerHTML+="<h3 style='display:inline'>"+str[idx]+"</h3>"
-console.log(str[idx]);
-console.log(str);
+paraEL.innerHTML+="<h3 style='display:inline'>"+str[idx]+"</h3>" //Printing to the Web Page
