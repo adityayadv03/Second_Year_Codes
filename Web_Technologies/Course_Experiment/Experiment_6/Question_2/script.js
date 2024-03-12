1b828846c856dc9b95588232840145c507472c54
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-03-12 19:06:31 +0530

    Completed the question

--- Web_Technologies/Course_Experiment/Experiment_6/Question_2/script.js
+++ Web_Technologies/Course_Experiment/Experiment_6/Question_2/script.js
@@ -1,6 +1,9 @@
 let but1EL=document.getElementById("but1");
 let but2EL=document.getElementById("but2");
 let but3EL=document.getElementById("but3");
+let but4EL=document.getElementById("but4");
+let but5EL=document.getElementById("but5");
+let but6EL=document.getElementById("but6");
 
 
 but1EL.addEventListener("click",function changeback(){
@@ -18,9 +21,18 @@ but3EL.addEventListener("click",function changeback(){
     let eleEL=document.getElementById("el3");
     eleEL.style.backgroundColor=color;
 });
-
-function changeback(ele){
-    let color=prompt("Enter Any Valid Color to Change");
-    let eleEL=document.getElementById(ele);
+but4EL.addEventListener("click",function changewebcolor(){
+    let color=prompt("Enter Any Valid Color to Change ");
+    let eleEL=document.getElementById("body");
     eleEL.style.backgroundColor=color;
-}
+})
+but5EL.addEventListener("click",function change(){
+    let color=prompt("Enter Any Valid Color to Change ");
+    let eleEL=document.getElementsByClassName("center");
+    for(let i=0 ; i<eleEL.length ; i++){
+        eleEL[i].style.backgroundColor=color;
+    }
+})
+but6EL.addEventListener("click",function alertbut(){
+    alert("Hey User Alert Message Show!");
+})
