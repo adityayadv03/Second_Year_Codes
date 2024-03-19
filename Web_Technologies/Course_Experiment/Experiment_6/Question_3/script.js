2c117bbd8a9e54d4c9c77a50d5f157f3c6c6c896
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-03-19 19:16:20 +0530

    Added Some Constraints

--- Web_Technologies/Course_Experiment/Experiment_6/Question_3/script.js
+++ Web_Technologies/Course_Experiment/Experiment_6/Question_3/script.js
@@ -1,26 +1,56 @@
 let dropEL=document.getElementById("age");
-for(let i=18 ; i<=50 ; i++){
+for(let i=12 ; i<=50 ; i++){
     dropEL.innerHTML+="<option>"+i+"</option>";
 }
 
 let nameEL=document.getElementById("name");
 let ageEl=document.getElementById("age");
 let messEl=document.getElementById("mess");
+let passEL=document.getElementById("pass");
 
-function valid(){
+function validFill(){
     let flag=false;
     if(nameEL.value!=="" && ageEl.value!==""){
         flag=true;
     }
     else{
         messEl.textContent="Please Enter the Name and Age Before Submitting";
+        butEL.disabled=true;
+    }
+    return flag;
+}
+
+function validAge(){
+    let flag=false;
+    if(ageEl.value>=18 && ageEl.value<=30){
+        flag=true;
+    }
+    else{
+        messEl.textContent="Age is Not in the Range 18-30 . ";
+        butEL.disabled=true;
+    }
+    return flag;
+}
+
+function validPass(){
+    let flag=false;
+    let password=passEL.value;
+    if(password.length>=6){
+        flag=true;
+    }
+    else{
+        messEl.textContent="Password is To Small . ";
+        butEL.disabled=true;
     }
     return flag;
 }
 
 let butEL=document.getElementById("sub");
 butEL.addEventListener("click",function check(){
-    if(valid()===true){
+    if(validFill()===true && validAge()===true && validPass()===true){
         document.write("Thank You For Submittion . ");
     }
+    setTimeout(()=>{
+        butEL.disabled=false;
+    },5000);
 })
