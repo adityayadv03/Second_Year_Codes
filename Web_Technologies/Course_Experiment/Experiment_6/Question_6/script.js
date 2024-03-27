eb468afbf8749d34e24279b628def12008378ee4
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-03-27 20:15:39 +0530

    Added invalid index Error in js code

--- Web_Technologies/Course_Experiment/Experiment_6/Question_6/script.js
+++ Web_Technologies/Course_Experiment/Experiment_6/Question_6/script.js
@@ -3,10 +3,25 @@ const strEL=document.getElementById("str");
 const endEL=document.getElementById("end");
 const extEL=document.getElementById("ext");
 const evaEL=document.getElementById("eva");
+const errorEL=document.getElementById("error");
 
 evaEL.addEventListener("click",()=>{
     let str=inputEL.value;
     let startindx=strEL.value;
     let endindx=endEL.value;
-    extEL.textContent=str+startindx+endindx;
+    let valid=str.length;
+    if(startindx<0 || endindx>=valid){
+        errorEL.textContent+="Invalid Index"
+        setTimeout(() => {
+            inputEL.value="";
+            strEL.value="";
+            endEL.value="";
+            errorEL.textContent="";
+        }, 3000);
+    }else{
+        extEL.textContent="";
+        for(let i=startindx ; i<=endindx ; i++){
+            extEL.textContent+=str.charAt(i);
+        }
+    }
 })
