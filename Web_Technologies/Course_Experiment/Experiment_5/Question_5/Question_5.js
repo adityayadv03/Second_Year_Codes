9085b803aa905338efb1a4f81b497edadc384994
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-04-11 16:03:36 +0530

    Modifing Folders and File Code

--- Web_Technologies/Course_Experiment/Experiment_5/Question_5/Question_5.js
+++ Web_Technologies/Course_Experiment/Experiment_5/Question_5/Question_5.js
@@ -5,8 +5,8 @@ let evenansEL=document.getElementById("evenans");
 let oddansEL=document.getElementById("oddans");
 
 function validinput(){
-    let low=lowEL.value;
-    let high=highEL.value;
+    let low=parseInt(lowEL.value);
+    let high=parseInt(highEL.value);
     let flag=true;
     if(low<0 || low>1000 || high<0 || high>1000 || high<low){
         flag=false;
@@ -22,7 +22,7 @@ function validinput(){
 function printIt(){
     let iseven=document.getElementById("even").checked;
     let isodd=document.getElementById("odd").checked;
-    for(let i=lowEL.value ; i<=highEL.value ; i++){
+    for(let i=parseInt(lowEL.value) ; i<=parseInt(highEL.value) ; i++){
         if(i%2==0 && iseven==true){
             oddansEL.textContent="";
             evenansEL.textContent+=i+" ";
