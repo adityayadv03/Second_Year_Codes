af2d410e6d3e2d086e8fbb1c958ab98304bcda11
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-04-29 21:52:12 +0530

    Targeting the form field

---
+++ Web_Technologies/Course_Experiment/Experiment_9/Question_2/script.js
@@ -0,0 +1,10 @@
+const nameEL=document.getElementById("name");
+const mailEl=document.getElementById("mail");
+const sapEL=document.getElementById("Sap");
+const submitEL=document.getElementById("submit");
+
+submitEL.addEventListener("click",(event)=>{
+    let name=nameEL.value;
+    let sap=sapEL.value;
+    let mail=mailEl.value; 
+})
